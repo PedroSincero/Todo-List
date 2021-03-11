e28d953192924e45c779a784af73c62c805c552c
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-03-11 15:50:41 -0300

    adicionando desafio 5

---
+++ script.js
@@ -0,0 +1,14 @@
+//adiciona uma LI
+
+let getButton = document.getElementById('criar-tarefa');
+let getTexto = document.getElementById('texto-tarefa');
+
+// Agradecimentos ao Site https://www.horadecodar.com.br/2020/12/15/como-pegar-valor-de-input-com-javascript/ 
+getButton.addEventListener('click',function(){
+
+let getOL = document.getElementById('lista-tarefas');
+let createLi = document.createElement('li');
+
+createLi.innerText = getTexto.value;
+getOL.appendChild(createLi);
+});
