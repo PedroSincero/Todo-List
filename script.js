58891e28bc1db4647d3ed5d81f3955897bc50b57
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-03-11 17:40:06 -0300

    adicionando desafio 7 & 8

--- script.js
+++ script.js
@@ -2,16 +2,37 @@
 
 let getButton = document.getElementById('criar-tarefa');
 let getTexto = document.getElementById('texto-tarefa');
-
+let getOL = document.getElementById('lista-tarefas');
 
 getButton.addEventListener('click',function(){
 
-let getOL = document.getElementById('lista-tarefas');
 let createLi = document.createElement('li');
-createLi.classList = 'listinha';
+createLi.classList = 'lista';
 createLi.innerText = getTexto.value;
 getOL.appendChild(createLi);
 getTexto.value = '';
 });
 
+getOL.addEventListener('click', function(event){
+    let getLista = document.getElementsByClassName('lista');
+    for(let index in getLista){
+        getLista[index].classList.remove('cinza');
+        event.target.classList.add('cinza');
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Agradecimentos ao Daniel Roberto Turma 10 Tribo B
 // Agradecimentos ao Site https://www.horadecodar.com.br/2020/12/15/como-pegar-valor-de-input-com-javascript/ ; https://cursos.alura.com.br/forum/topico-limpando-a-caixa-de-texto-20395
