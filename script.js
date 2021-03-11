6f28850a918f051ee675aee285cc0aab96875e38
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-03-11 16:50:46 -0300

    adicionando correçoes ao desafio 5

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let getButton = document.getElementById('criar-tarefa');
 let getTexto = document.getElementById('texto-tarefa');
 
-// Agradecimentos ao Site https://www.horadecodar.com.br/2020/12/15/como-pegar-valor-de-input-com-javascript/ 
+
 getButton.addEventListener('click',function(){
 
 let getOL = document.getElementById('lista-tarefas');
@@ -11,4 +11,7 @@ let createLi = document.createElement('li');
 createLi.classList = 'listinha';
 createLi.innerText = getTexto.value;
 getOL.appendChild(createLi);
+getTexto.value = '';
 });
+
+// Agradecimentos ao Site https://www.horadecodar.com.br/2020/12/15/como-pegar-valor-de-input-com-javascript/ ; https://cursos.alura.com.br/forum/topico-limpando-a-caixa-de-texto-20395
