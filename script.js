01dda2c39bd0a24e9016d63c54304e34359d1afb
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-03-11 18:43:42 -0300

    adicionando ajustes no desafio 7 & 8

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-//adiciona uma LI
-
 let getButton = document.getElementById('criar-tarefa');
 let getTexto = document.getElementById('texto-tarefa');
 let getOL = document.getElementById('lista-tarefas');
 
 getButton.addEventListener('click',function(){
-
 let createLi = document.createElement('li');
-createLi.classList = 'lista';
-createLi.innerText = getTexto.value;
-getOL.appendChild(createLi);
-getTexto.value = '';
+    createLi.classList = 'lista';
+    createLi.innerText = getTexto.value;
+    getOL.appendChild(createLi);
+    getTexto.value = '';
 });
 
 getOL.addEventListener('click', function(event){
     let getLista = document.getElementsByClassName('lista');
-    for(let index in getLista){
+    for(let index = 0; index < getLista.length; index+=1){
+        console.log(index);
         getLista[index].classList.remove('cinza');
         event.target.classList.add('cinza');
     }
 });
 
-//Agradecimentos ao Daniel Roberto Turma 10 Tribo B
 // Agradecimentos aos Sites https://www.horadecodar.com.br/2020/12/15/como-pegar-valor-de-input-com-javascript/ ; https://cursos.alura.com.br/forum/topico-limpando-a-caixa-de-texto-20395
+//Agradecimentos ao Daniel Roberto Turma 10 Tribo B
